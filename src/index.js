64f60f8b07aaaa6989b6a2221fd3d49e36a7160a
TurtleSun <57500228+TurtleSun@users.noreply.github.com>
2020-09-24 11:51:37 -0400

    .

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 
+//give user input AKA get bill
+const tipForm = document.querySelector("#tip-form"); //global scope
+// ORRRR const tipForm = document.getElementById("tip-form");
+
+
+tipForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const billAmount = Number(document.querySelector("#bill-amount").value); 
+    //concept called scope to function, function scoped
+    const tipPercentage = Number(document.querySelector("#tip-amount").value);
+    const tipAmount = document.querySelector("#final-tip");
+    tipAmount.innerHTML = "50";
+    //`${tipPercent}`
+
+    //insert math and 
+});
+
+//all html are documents and css are selectors
+
+//get tip percentage
+
+
+//process values (combine n calcuate)
+
+
+//display to user
